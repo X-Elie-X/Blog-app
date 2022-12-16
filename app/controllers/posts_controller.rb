1ff98baededdb3906c250a116198077ae6e94682
X-Elie-X <103565978+X-Elie-X@users.noreply.github.com>
2022-12-17 01:49:20 +0200

    create a view page and index page for user and post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,10 @@
 class PostsController < ApplicationController
   def index
     @user = User.find_by(id: params[:id])
+    @post = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
+    @posts = Post.find(params[:id])
   end
 end
