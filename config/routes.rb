9a9372a54216f7f21ab0c04cfb4af975264a4111
X-Elie-X <103565978+X-Elie-X@users.noreply.github.com>
2022-12-17 22:20:19 +0200

    add the form for the comment and likes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
 
-  root to: 'user#index'
-  resources :user, only: [:index, :show] do
-    resources :posts, only: [:index, :show] 
-  end
+  root to: 'users#index'
+  resources :user, only: %i[index show] do
+    resources :posts, only: %i[index show new create] do
+      resources :comments, only: %i[new create] 
+      resources :likes, only: %i[new create]
+    end
+ end
+
 
 end
